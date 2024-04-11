c26cf7720eea8258780a6149218694f723cfd169
Alexis Fernandez <alexismatiasfernandez@gmail.com>
2024-04-11 10:17:02 -0300

    Add new Prroject Pruebas

---
+++ Com_Socket/Cliente/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente;
+class Cliente
+{
+    public static void Main()
+    {
+        TcpClient clientSocket = new TcpClient();
+        clientSocket.Connect("127.0.0.1", 8888);
+        Console.WriteLine(" >> Conexión Establecida !!!");
+        NetworkStream serverStream = clientSocket.GetStream();
+
+        string mensaje = Console.ReadLine();
+
+        byte[] outStream = Encoding.ASCII.GetBytes(mensaje + "$");
+        serverStream.Write(outStream, 0, outStream.Length);
+        serverStream.Flush();
+
+        byte[] inStream = new byte[10025];
+        serverStream.Read(inStream, 0, inStream.Length);
+        string returnData = Encoding.ASCII.GetString(inStream);
+        Console.WriteLine(" >> " + returnData);
+
+        clientSocket.Close();
+
+    }
+}
---
+++ Com_Socket/Servidor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor;
+class Servidor
+{
+    public static void Main()
+    {
+        // Se define la conexión inicial (IP + Puerto)
+
+        IPAddress direccionIP = IPAddress.Parse("127.0.0.1");
+        TcpListener servidorSocket = new TcpListener(direccionIP, 8888);
+        TcpClient clienteSocket = default(TcpClient);
+
+
+        // Se inicializa el Servidor y se establece la conexión con el Cliente
+
+        servidorSocket.Start();
+        Console.WriteLine(" >> Servidor Iniciado");
+        Console.WriteLine(" >> Esperando la vinculación con el Cliente...");
+        clienteSocket = servidorSocket.AcceptTcpClient();
+        Console.WriteLine(" >> Cliente conectado !!!");
+        NetworkStream networkStream = clienteSocket.GetStream();
+
+
+        // Se administra el buffer con los datos enviados por el Cliente y se traducen los bits recibidos
+        // en un mensaje legible almacenado en un string.
+
+        byte[] bytesFrom = new byte[10025];
+        networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+        string dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+
+        // Envía al Cliente un aviso del último mensaje recibido codificado en un array de Byte
+
+        Console.WriteLine(" >> Datos del Cliente - " + dataFromClient);
+        string serverResponse = "Ultimo mesaje del Cliente RECIBIDO. Fin de la Conexion !";
+        Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+        networkStream.Write(sendBytes, 0, sendBytes.Length);
+        networkStream.Flush();
+
+
+        // Cierra la comunicaci+on con el Cliente y los procesos activos asociados
+
+        clienteSocket.Close();
+        servidorSocket.Stop();
+        Console.WriteLine(" >> Salida de aplicación...");
+        Console.ReadLine();
+
+    }
+}
